a8c1002a9c8fd141672f18f29292a9cb74939202
rodriBRU1 <Dbrunelrodri@gmail.com>
2025-11-13 23:24:19 -0500

    first commit

---
+++ src/main/java/com/waveheaven/back/BackendWaveHeavenApplication.java
@@ -0,0 +1,13 @@
+package com.waveheaven.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWaveHeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWaveHeavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waveheaven/back/BackendWaveHeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waveheaven.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWaveHeavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
